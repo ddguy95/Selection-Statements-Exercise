ac4aa4e752789bf660d53f06daa7b05efe563a46
derrickdrye <dryd95@hotmail.com>
2023-06-21 14:25:19 -0500

    completion of exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -2,9 +2,30 @@
 {
     public class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //RandomNumberGuessingGame
+
+            var r = new Random();
+            //var favNumber = r.Next(1, 1000);
+            var favNumber = 999;
+
+            Console.WriteLine("Try to guess my favorite number from 1 - 1000");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"{userInput} is too low.");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine($"{userInput} is too high.");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it. Great job!");
+            }
         }
     }
 }
